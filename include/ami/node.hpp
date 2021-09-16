36af9900530aa9ded23233659afebb176f0156d9
kena <kena.e8ac99@gmail.com>
2021-09-16 18:31:17 +0900

    feat:Add node class that holds weights

---
+++ include/ami/node.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <array>
+#include <concepts>
+#include <functional>
+#include <ranges>
+#include <span>
+
+#include "ami/concepts/execution_policy.hpp"
+#include "ami/concepts/optimizer.hpp"
+#include "ami/utility/indices.hpp"
+#include "ami/utility/parallel_algorithm.hpp"
+
+namespace ami {
+
+  template <std::size_t N, std::floating_point T = float>
+  class node final {
+  public:
+    // Public Types
+    using size_type = std::size_t;
+    using real_type = T;
+
+    using value_type = std::array<real_type, N>;
+
+    // Static Members
+    static constexpr size_type size = N;
+
+    // Constructor
+    node() = default;
+
+    explicit constexpr node(std::span<const real_type, size> value) {
+      std::ranges::copy(value, value_.begin());
+    }
+
+    // Public Methods
+    template <execution_policy auto P = std::execution::seq>
+    constexpr real_type forward(std::span<const real_type, size> input) const {
+      return utility::transform_reduce<P>(value_, input, real_type{});
+    }
+
+    template <execution_policy auto P = std::execution::seq>
+    constexpr value_type backward(real_type delta) const {
+      value_type output{};
+      utility::transform<P>(
+          value_, output.begin(), std::bind_front(std::multiplies{}, delta));
+      return output;
+    }
+
+    template <execution_policy auto P = std::execution::seq,
+              optimizer<real_type>  O>
+    constexpr void update(std::span<O, size>               optimizers,
+                          std::span<const real_type, size> gradients) {
+      utility::for_each<P>(
+          utility::indices<size>,
+          [=, this](auto i) { optimizers[i](value_[i], gradients[i]); });
+    }
+
+    // Getter / Setter
+    constexpr std::span<real_type, size> value() & noexcept {
+      return std::span{value_};
+    }
+
+    constexpr std::span<const real_type, size> value() const& noexcept {
+      return std::span{value_};
+    }
+
+    constexpr value_type value() && noexcept {
+      return std::move(value_);
+    }
+  private:
+    // Private Members
+    value_type value_{};
+
+    // Private Methods
+  };
+}
---
+++ test/src/node.cc
@@ -0,0 +1,74 @@
+#include "ami/node.hpp"
+
+#include <boost/ut.hpp>
+
+int main() {
+  using namespace boost::ut;
+
+  constexpr std::size_t size = 3;
+
+  constexpr ami::node<size> src{std::array<float, size>{1.0f, 0.1f, 0.01f}};
+
+  constexpr std::array<float, size> input{0.1f, 0.2f, 0.3f};
+
+  constexpr float delta = 0.1f;
+
+  "forward"_test =
+    [&]() {
+      constexpr auto output = src.forward(input);
+
+      expect(eq(output, 0.123_f));
+    };
+
+  "parallel_forward"_test =
+    [&]() {
+      const auto output = src.forward<std::execution::par_unseq>(input);
+
+      expect(eq(output, 0.123_f));
+    };
+
+  "backward"_test =
+    [&]() {
+      constexpr auto output = src.backward(delta);
+
+      expect(eq(output[0], 0.1_f));
+      expect(eq(output[1], 0.01_f));
+      expect(eq(output[2], 0.001_f));
+    };
+
+  "parallel_backward"_test =
+    [&]() {
+      const auto output = src.backward<std::execution::par_unseq>(delta);
+
+      expect(eq(output[0], 0.1_f));
+      expect(eq(output[1], 0.01_f));
+      expect(eq(output[2], 0.001_f));
+    };
+
+  constexpr auto test_optimizer =
+    [](auto& weight, auto gradient) { weight += gradient; };
+
+  std::array optimizers{test_optimizer, test_optimizer, test_optimizer};
+
+  "update"_test =
+    [&, src = ami::node(src)]() mutable {
+      src.update(std::span{optimizers}, std::span{input});
+
+      const auto output = src.value();
+
+      expect(eq(output[0], 1.1_f));
+      expect(eq(output[1], 0.3_f));
+      expect(eq(output[2], 0.31_f));
+    };
+
+  "parallel_update"_test =
+    [&, src = ami::node(src)]() mutable {
+      src.update<std::execution::par_unseq>(std::span{optimizers},
+                                            std::span{input});
+      const auto output = src.value();
+
+      expect(eq(output[0], 1.1_f));
+      expect(eq(output[1], 0.3_f));
+      expect(eq(output[2], 0.31_f));
+    };
+}
