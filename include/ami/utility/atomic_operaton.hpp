3c2d41047d058c2b997c2c0f48c39278afc97f4f
kena <kena.e8ac99@gmail.com>
2021-09-17 15:11:33 +0900

    feat:Add utility function for atomic operation

---
+++ include/ami/utility/atomic_operaton.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <atomic>
+
+#include "ami/concepts/execution_policy.hpp"
+
+namespace ami::utility {
+
+  template <execution_policy auto P = std::execution::seq, typename T>
+  requires std::atomic_ref<T>::is_always_lock_free
+  inline constexpr T fetch_add(
+      T& t,
+      typename std::atomic_ref<T>::difference_type operand,
+      [[maybe_unused]] std::memory_order order = std::memory_order_seq_cst) {
+    if constexpr (std::common_with<
+                    decltype(P), std::execution::sequenced_policy>) {
+      T temp = t;
+      t += operand;
+      return temp;
+    }
+    else {
+      return std::atomic_ref<T>{t}.fetch_add(operand, order);
+    }
+  }
+}
