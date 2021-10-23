27a1b3a0a5257ea15c456fc99ba34636a468a792
kena <kena.e8ac99@gmail.com>
2021-10-23 16:06:11 +0900

    feat:Add concept for layer

---
+++ include/ami/concepts/layer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ami/concepts/abstract_layer.hpp"
+
+namespace ami {
+  template <class T>
+  concept layer = abstract_layer<T> ||
+                  requires (T t) {
+                    typename T::input_type;
+                    typename T::forward_type;
+                    typename T::backward_type;
+                    typename T::delta_type;
+                    typename T::gradient_type;
+
+                    {T::input_size}  -> std::same_as<typename T::size_type>;
+                    {T::output_size} -> std::same_as<typename T::size_type>;
+                  };
+}
