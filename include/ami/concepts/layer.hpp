27ed0f3c7119a383e7becfe9d77a78913ed9bc9b
kena <kena.e8ac99@gmail.com>
2021-10-30 14:36:03 +0900

    fix:Fix to "common_reference_with" from "same_as"

--- include/ami/concepts/layer.hpp
+++ include/ami/concepts/layer.hpp
@@ -4,15 +4,16 @@
 
 namespace ami {
   template <class T>
-  concept layer = abstract_layer<T> ||
-                  requires (T t) {
-                    typename T::input_type;
-                    typename T::forward_type;
-                    typename T::backward_type;
-                    typename T::delta_type;
-                    typename T::gradient_type;
+  concept layer
+  = abstract_layer<T> ||
+    requires (T t) {
+      typename T::input_type;
+      typename T::forward_type;
+      typename T::backward_type;
+      typename T::delta_type;
+      typename T::gradient_type;
 
-                    {T::input_size}  -> std::same_as<typename T::size_type>;
-                    {T::output_size} -> std::same_as<typename T::size_type>;
-                  };
+      {T::input_size}  -> std::common_reference_with<typename T::size_type>;
+      {T::output_size} -> std::common_reference_with<typename T::size_type>;
+    };
 }
