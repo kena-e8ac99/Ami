ba4de39be17d019e9bf5b29f616e4fd9aa3cb9da
kena <kena.e8ac99@gmail.com>
2021-11-20 15:48:51 +0900

    fix:Minor fix to remove ifdef macro

--- include/ami/activation_function/sigmoid.hpp
+++ include/ami/activation_function/sigmoid.hpp
@@ -7,20 +7,12 @@ namespace ami {
 
   struct sigmoid final {
     template <std::floating_point T>
-    static
-#ifdef __GNUC__
-    constexpr
-#endif
-    T f(T t) {
+    static constexpr T f(T t) {
       return 1 / (1 + std::exp(-t));
     }
 
     template <std::floating_point T>
-    static
-#ifdef __GNUC__
-      constexpr
-#endif
-    T df(T t) {
+    static constexpr T df(T t) {
       const auto tmp = f(t);
       return tmp * (1 - tmp);
     }
