7cb0e6ef59e82230662d354342b21436c9970f29
kena <kena.e8ac99@gmail.com>
2022-01-26 22:12:56 +0900

    feat:Determine member types of 'node' class

--- include/ami/node.hpp
+++ include/ami/node.hpp
@@ -1,5 +1,21 @@
 #pragma once
 
-namespace ami {
+#include <array>
+#include <concepts>
+#include <cstddef>
+#include <type_traits>
 
+namespace ami {
+  template<std::floating_point RealType, std::size_t Size>
+  requires (Size > 0)
+  class node final {
+  public:
+    // Public Types
+    using size_type     = std::size_t;
+    using real_type     = RealType;
+    using value_type    =
+        std::conditional_t<Size == 1, real_type, std::array<RealType, Size>>;
+    using forward_type  = real_type;
+    using backward_type = value_type;
+  };
 }
--- test/src/node.cc
+++ test/src/node.cc
@@ -1,6 +1,35 @@
 #include "ami/node.hpp"
 
+#include <array>
+#include <concepts>
+#include <cstddef>
+#include <tuple>
+#include <utility>
+
 #include "boost/ut.hpp"
 
 int main() {
+  using namespace boost::ut;
+  using namespace ami;
+
+  "type check"_test = []<class RealType>() {
+    static_assert(std::same_as<
+        typename node<RealType, 1>::size_type, std::size_t>);
+
+    static_assert(std::same_as<
+        typename node<RealType, 1>::real_type, RealType>);
+
+    static_assert(std::same_as<
+        typename node<RealType, 1>::value_type, RealType>);
+    static_assert(std::same_as<
+        typename node<RealType, 2>::value_type, std::array<RealType, 2>>);
+
+    static_assert(std::same_as<
+        typename node<RealType, 1>::forward_type, RealType>);
+
+    static_assert(std::same_as<
+        typename node<RealType, 1>::backward_type, RealType>);
+    static_assert(std::same_as<typename
+        node<RealType, 2>::backward_type, std::array<RealType, 2>>);
+  } | std::pair<float, double>{};
 }
